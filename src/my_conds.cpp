ace8b855a3f4d694f514c26e53c1febd253184e6
Crandel <cradlemann@gmail.com>
2018-12-22 23:58:37 +0100

    Finish conditions

--- src/my_conds.cpp
+++ src/my_conds.cpp
@@ -15,4 +15,25 @@ void can_drive(unsigned int age, unsigned int last_exam, bool is_toxicated){
   } else {
     cout << "You could drive" << endl;
   }
+
+  int some_options = 2;
+
+  switch(some_options) {
+    case 1:
+      cout << "Hello!" << endl;
+      break; // very important, stop comparing
+    case 2:
+      cout << "Hallo!" << endl;
+      break;
+    case 3:
+      cout << "Bonjour!" << endl;
+      break;
+
+    default:
+      cout << "Здрасти!" << endl;
+  }
+
+  int ternary = (3 > 5) ? 3 : 5;
+  cout << "Ternary operator is " << ternary << endl;
+  cout << "********************************" << endl;
 }
